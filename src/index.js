8dde75e7c80917c8091845e7f4646ab90814caff
romax114 <70773814+romax114@users.noreply.github.com>
2020-10-25 23:16:27 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (m = []) {
+    if(!m.length) return [];
+    m.map(a => {if(m.indexOf(a) % 2) a.reverse()});
+    return m.flat();
 }
